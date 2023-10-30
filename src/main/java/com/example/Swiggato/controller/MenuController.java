4aa2fdef5fd1a274a168b75e2cdaa50f3f5209eb
missshikhamishra <118651701+missshikhamishra@users.noreply.github.com>
2023-10-30 19:43:39 +0530

    Update MenuController.java

--- src/main/java/com/example/Swiggato/controller/MenuController.java
+++ src/main/java/com/example/Swiggato/controller/MenuController.java
@@ -7,17 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/menu")
 public class MenuController {
 
-    // get all foods of a particualr category
-
-    // get all MAIN_COURSE items with price above x rupees from a particular restaurant.
-
-    // get all veg foods of a restaurant
-
-    // get all non veg foods of a restaurant
-
-    // Get cheapest 5 food items of a partiuclar restaurant
-
-    // Get costliest 5 food items of a partiuclar restaurant
-
-    // Get costliest 5 food items of a partiuclar catgeory -> name fo dish and rest which serves that dish
 }
